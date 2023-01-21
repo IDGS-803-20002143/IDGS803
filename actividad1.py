5057c2caef700a473bf90ae58140624117a72680
IDGS-803-20002143 <20002143@alumnos.utleon.edu.mx>
2023-01-21 17:35:02 -0600

    actividad 1

--- actividad1.py
+++ actividad1.py
@@ -1,63 +1,8 @@
 
-def menu():
-    print("App de operaciones.")
-    print("1. Suma")
-    print("2. Resta")
-    print("3. Multiplicacion")
-    print("4. Division")
-    print("5. Salir")
+def multiplicar():
+    print("multiplicar")
+    num1=int(input("dame un numero: "))
 
-    op = input ("Escriba la opcion:")
-
-    op = int(op)
-
-    if op==1: suma()
-    elif op==2: resta()
-    elif op==3: multiplicacion()
-    elif op==4: division()
-    else: exit("hasta luego")
-
-    def suma():
-     print("")
-     print ("suma")
-     num1 = input("Numero 1: ")
-     num2 = input("Numero 2: ")
-     num1 = float(num1)
-     num2 = float(num2)
-     print ("Resultado: ", num1+num2)
-     print("")
-     menu()
-    
-def resta():
-    print("")
-    print ("resta.")
-    num1 = input("Numero 1: ")
-    num2 = input("Numero 2: ")
-    num1 = float(num1)
-    num2 = float(num2)
-    print ("Resultado: ", num1-num2)
-    print("")
-    menu()
-
-def multiplicacion():
-    print("")
-    print ("multiplicacion.")
-    num1 = input("Numero 1: ")
-    num2 = input("Numero 2: ")
-    num1 = float(num1)
-    num2 = float(num2)
-    print ("Resultado: ", num1*num2)
-    print("")
-    menu()
-
-def division():
-    print("")
-    print ("division.")
-    num1 = input("Numero 1: ")
-    num2 = input("Numero 2: ")
-    num1 = float(num1)
-    num2 = float(num2)
-    print ("Resultado: ", num1/num2)
-    print("")
-    menu()
-menu()
+    for x in range(1,11):
+        print("{} * {} = {}".format(num1,x,(num1*x)))
+multiplicar()
