c669a2db53a92687f06cbb4930dc4a6c406e4c75
IDGS-803-20002143 <20002143@alumnos.utleon.edu.mx>
2023-01-21 17:55:42 -0600

    operaciones

---
+++ tema2-operacionesmatematicas.py
@@ -0,0 +1,9 @@
+num1 = 20
+num2 = 2
+
+print("Suma: ", (num1 + num2))
+print("Resta: ", (num1 - num2))
+print("Multiplicar: ", (num1 * num2))
+print("Dividir: ", (num1/num2))
+print("Division exacta: ", (num1 // num2))
+print("Potencia: ", (num1 ** num2))
