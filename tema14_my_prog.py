041971d3f262543c3254fd749b3300d738fad991
IDGS-803-20002143 <20002143@alumnos.utleon.edu.mx>
2023-01-21 18:25:38 -0600

    my prog

---
+++ tema14_my_prog.py
@@ -0,0 +1,14 @@
+def tem():
+    print("Hola desde my_prog.py!!!")
+
+def tem2():
+    print("Adiós desde my_prog.py!!!")
+
+def main():
+    print("Soy una función y hago algo!")
+
+    tem()
+    tem2()
+
+if __name__=="__main__":
+    main()
